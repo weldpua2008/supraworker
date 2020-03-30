dcb02f532b7221e30d6f14f7ba4208e411fa1823
Valeriy Solovyov <weldpua2008@gmail.com>
2020-03-30 14:53:01 +0300

    [bugfix] Remove finished jobs from registry

--- worker/worker.go
+++ worker/worker.go
@@ -35,30 +35,7 @@ func StartWorker(id int, jobs <-chan *model.Job, wg *sync.WaitGroup) {
 			log.Debug(fmt.Sprintf("Job %v finished in %v", j.Id, dur))
 			j.Finish()
 		}
-		job.JobsRegistry.Delete(j.Id)
+		job.JobsRegistry.Delete(j.StoreKey())
 
 	}
-	// for {
-	//     log.Debug(fmt.Sprintf("Jobs has size %v", len(jobs)))
-	// 	select {
-	// 	case j, more := <-jobs:
-	// 		if more {
-	// 			log.Info(fmt.Sprintf("Worker %v received Job %v ", id, j.Id))
-	// 			if err := j.Run(); err != nil {
-	// 				log.Info(fmt.Sprintf("Job %v failed with %s", j.Id, err))
-	// 				j.Cancel()
-	// 			} else {
-	// 				dur := time.Now().Sub(j.StartAt)
-	// 				log.Debug(fmt.Sprintf("Job %v finished in %v", j.Id, dur))
-	// 				j.Finish()
-	// 			}
-	// 			job.JobsRegistry.Delete(j.Id)
-	// 		} else {
-	//             log.Info(fmt.Sprintf("Worker %v received all jobs",id))
-	// 			// received all jobs
-	// 			return
-	// 		}
-	// 	}
-	// }
-
 }
