70f0be138fc8c08a9a9a71f694e5f1cdd3608187
Valeriy Soloviov <weldpua2008@gmail.com>
2021-04-20 18:25:33 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#
+# git clone https://github.com/weldpua2008/supraworker.git
+# cd supraworker
+#
 cd "$(dirname "$0}")" || exit 1
 cd ..
 docker run -ti --rm   -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.15.11
